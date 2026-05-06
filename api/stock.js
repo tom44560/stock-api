2cde6a1c7e33ad9611fa3acf240674b56c3cb38c
tom44560 <tom44560@gmail.com>
2026-05-06 22:51:49 +0800

    Delete api/stock.js

--- api/stock.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  const { symbol } = req.query;
-  const url = `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo=${symbol}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  res.json(data);
-}
