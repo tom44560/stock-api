1b2bc8a139996f0995039f969f949eedd72c8d60
tom44560 <tom44560@gmail.com>
2026-05-06 21:31:24 +0800

    Fix export syntax in stock.js

--- stock.js
+++ stock.js
@@ -1,4 +1,5 @@
-export default async function handler(req, res) {
+｛
+  export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   const { symbol } = req.query;
   const url = `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo=${symbol}`;
